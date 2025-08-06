299975db12fa8ca3be4537c65d66b23349be6cbd
myoungjinseo <sih5673@gmail.com>
2025-08-06 14:38:17 +0900

    test : 빌드

--- exam4/src/test/java/com/back/WiseSayingControllerTest.java
+++ exam4/src/test/java/com/back/WiseSayingControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.io.File;
 import java.io.IOException;
 
 import static com.back.AppTest.run;
@@ -116,5 +117,40 @@ class WiseSayingControllerTest {
                 .contains("번호 / 작가 / 명언")
                 .contains("----------------------")
                 .contains("2 / 홍길동 / 현재와 자신을 사랑하라.");
+    }
+
+    @Test
+    @DisplayName("빌드")
+    void createAllWiseSayingJson() throws IOException {
+
+        File file = new File("src/main/java/com/back/db/wiseSaying/data.json");
+
+        final String out = run(
+                """
+                      등록
+                      현재를 사랑하라.
+                      작자미상
+                      등록
+                      과거에 집착하지 마라.
+                      작자미상
+                      목록
+                      삭제?id=1
+                      삭제?id=1
+                      수정?id=3
+                      수정?id=2
+                      현재와 자신을 사랑하라.
+                      홍길동
+                      목록
+                      빌드
+                      종료
+                      """);
+
+        assertThat(out)
+                .contains("data.json 파일의 내용이 갱신되었습니다.");
+
+        assertThat(file)
+                .exists()
+                .isFile();
+
     }
 }
