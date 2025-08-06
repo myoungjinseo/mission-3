ab802542751d6b0a7ed58204ac3d378a5bcb8aaf
myoungjinseo <sih5673@gmail.com>
2025-08-06 11:02:32 +0900

    test : 목록

--- exam4/src/test/java/com/back/WiseSayingControllerTest.java
+++ exam4/src/test/java/com/back/WiseSayingControllerTest.java
@@ -24,4 +24,27 @@ class WiseSayingControllerTest {
                 .contains("작가 :")
                 .contains("1번 명언이 등록되었습니다.");
     }
+
+    @Test
+    @DisplayName("목록")
+    void getWiseSayingTest(){
+
+        final String out = AppTest.run(
+                """
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                종료
+                """);
+
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("----------------------")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
