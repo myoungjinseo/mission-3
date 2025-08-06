7aa2da460ace8b9eb6d8fe7a343cbc540a88ac9f
myoungjinseo <sih5673@gmail.com>
2025-08-06 11:55:30 +0900

    test : 삭제

--- exam4/src/test/java/com/back/WiseSayingControllerTest.java
+++ exam4/src/test/java/com/back/WiseSayingControllerTest.java
@@ -47,4 +47,26 @@ class WiseSayingControllerTest {
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
                 .contains("1 / 작자미상 / 현재를 사랑하라.");
     }
+
+    @Test
+    @DisplayName("삭제")
+    void deleteWiseSayingTest(){
+        final String out = AppTest.run(
+                """
+                      등록
+                      현재를 사랑하라.
+                      작자미상
+                      등록
+                      과거에 집착하지 마라.
+                      작자미상
+                      목록
+                      삭제?id=1
+                      삭제?id=1
+                      종료
+                      """);
+
+        assertThat(out)
+                .contains("1번 명언이 삭제되었습니다.")
+                .contains("1번 명언은 존재하지 않습니다.");
+    }
 }
