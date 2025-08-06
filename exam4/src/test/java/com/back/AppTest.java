9e778c405b92a41c2ec2234d3d479c6956f9d99d
myoungjinseo <sih5673@gmail.com>
2025-08-06 11:37:51 +0900

    docs : TestUtil에 메소드 사용 이유 주석

--- exam4/src/test/java/com/back/AppTest.java
+++ exam4/src/test/java/com/back/AppTest.java
@@ -6,10 +6,12 @@ class AppTest {
 
     public static String run(String in) {
 
+        // app.run 실행되는 동안 콘솔 출력을 막아준다.
         ByteArrayOutputStream out = TestUtil.setOutToByteArray();
         App app = new App();
         app.sc = TestUtil.genScanner(in);
         app.run();
+        // app.run 실행이 중단되어 콘솔 출력으로 변경해준다.
         TestUtil.clearSetOutToByteArray(out);
 
         return out.toString();
